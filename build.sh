679a63b4edb2effca0e0034bb073cd8ba92cc456
12Me21 <12Me21.mc@gmail.com>
2020-08-18 21:02:24 -0400

    update build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cat fonts/fonts.css style.css markup.css code.css > _build.css
-cat fill.js entity.js request.js view.js navigate.js main.js > _build.js
+cat resource/fonts.css style.css markup.css code.css > _build.css
+cat fill.js entity.js request.js draw.js view.js navigate.js main.js > _build.js
 
 date=`date +%s`
 sed '/<!--START-->/,/<!--END-->/c<link rel="stylesheet" href="_build.css?'"$date"'">\
