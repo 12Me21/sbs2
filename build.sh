49816f5a6e520a1c64b3f127fe788a4fc27a0a9e
12Me21 <12Me21.mc@gmail.com>
2020-08-19 21:49:40 -0400

    add file in parser

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cat fonts.css style.css markup.css code.css > _build.css
-cat fill.js entity.js request.js draw.js view.js navigate.js main.js > _build.js
+cat sbs2-markup/sbhighlight.js sbs2-markup/parse.js sbs2-markup/render.js fill.js entity.js request.js draw.js view.js navigate.js main.js > _build.js
 
 date=`date +%s`
 sed '/<!--START-->/,/<!--END-->/c<link rel="stylesheet" href="_build.css?'"$date"'">\
