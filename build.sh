73b0026f256ea7af9a48c28dd74a41000ad7eb56
12Me21 <12Me21.mc@gmail.com>
2020-09-08 01:01:55 -0400

    move page to separate file

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ echo 'creating _build.css' >&2
 cat resource/fonts.css style.css markup.css code.css > resource/_build.css
 
 echo 'creating _build.js' >&2
-cat sbs2-markup/_build.js fill.js entity.js request.js draw.js view.js Views/settings.js Views/chat.js navigate.js main.js > resource/_build.js
+cat sbs2-markup/_build.js fill.js entity.js request.js draw.js view.js Views/settings.js Views/chat.js Views/page.js navigate.js main.js > resource/_build.js
 
 echo 'creating _build.html' >&2
 date=`date +%s`
--- view.js
+++ view.js
@@ -80,25 +80,6 @@ views: {
 			nav.appendChild(Draw.navButtons())
 		}
 	},
-	page: {
-		start: function(id, query, render) {
-			return $.Req.getPageView(id, render)
-		},
-		className: 'pageMode',
-		render: function(page) {
-			Nav.link("chat/"+page.id, $pageChatLink)
-			setEntityTitle(page)
-			setEntityPath(page)
-			$pageContents.replaceChildren(Parse.parseLang(page.content, page.values.markupLang))
-			Nav.link("editpage/"+page.id, $editButton)
-			if (/u/.test(page.myPerms))
-				flag('canEdit', true)
-		},
-		cleanUp: function() {
-			$pageContents.replaceChildren()
-			flag('canEdit', false)
-		}
-	},
 	category: {
 		start: function(id, query, render) {
 			return $.Req.getCategoryView(id, render)
