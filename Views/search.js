49c7394edc999ce37e9cda6ed889839c3bab2fa5
12Me21 <12Me21.mc@gmail.com>
2020-09-08 21:25:33 -0400

    fix search setting title in init

--- Views/search.js
+++ Views/search.js
@@ -3,12 +3,11 @@ with (View) (function($) { "use strict" //*/
 
 addView('search', {
 	init: function() {
-		setTitle("Search")
 	},
 	className: 'searchMode',
 	// todo: maybe support search params in url, and perform search immediately?
 	render: function(id, query, render) {
-		
+		setTitle("Search")
 	},
 	cleanUp: function() {
 		$searchResults.replaceChildren()
