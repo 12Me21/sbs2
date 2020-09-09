22e1d4c1047a159b05220a1bc7e6993558213dc6
12Me21 <12Me21.mc@gmail.com>
2020-09-08 21:23:37 -0400

    template view, unused

---
+++ Views/template.js
@@ -0,0 +1,17 @@
+<!--/* trick indenter
+with (View) (function($) { "use strict" //*/
+
+addView('template', {
+	init: function() {
+	},
+	start: function(id, query, render) {
+	},
+	className: '',
+	render: function() {
+	},
+	cleanUp: function() {
+	},
+})
+
+<!--/*
+}(window)) //*/ // pass external values
