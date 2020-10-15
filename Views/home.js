e6d6f4cc5ffadf4ce2b502ae0089acce9af26ed0
12Me21 <12Me21.mc@gmail.com>
2020-10-15 01:31:30 -0400

    split homepage view

---
+++ Views/home.js
@@ -0,0 +1,17 @@
+<!--/* trick indenter
+with (View) (function($) { "use strict" //*/
+
+addView('', {
+	init: function() {
+	},
+	className: 'homeMode',
+	render: function() {
+		var text = "Welcome to SmileBASIC Source 2!"
+		setTitle(text)
+	},
+	cleanUp: function() {
+	},
+})
+
+<!--/*
+}(window)) //*/ // pass external values
