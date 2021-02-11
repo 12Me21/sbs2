5fb98f894a94e9010087b75fd686a87d933b8cee
12Me21 <12Me21.mc@gmail.com>
2021-02-10 19:46:25 -0500

    undo

--- sidebar.js
+++ sidebar.js
@@ -209,7 +209,7 @@ displayMessages: function(comments, initial) {
 },
 
 limitMessages: function() {
-	var x = scroller.scrollBottom
+	//var x = scroller.scrollBottom
 	while (displayedMessages > 500) {
 		var n = scroller.inner.firstChild
 		var id = n.getAttribute('data-id')
@@ -218,7 +218,7 @@ limitMessages: function() {
 		n.remove()
 		displayedMessages--
 	}
-	scroller.scrollBottom = x
+	//scroller.scrollBottom = x
 }
 
 <!--/* 
