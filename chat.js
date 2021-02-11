be67bd83e1ac645f1cc89b68b93afd5af1ec9451
12Me21 <12Me21.mc@gmail.com>
2021-02-10 19:46:25 -0500

    undo

--- chat.js
+++ chat.js
@@ -211,14 +211,14 @@ ChatRoom.prototype.removeMessage = function(id) {
 	
 	var parent = message.parentNode
 
-	var x = this.scroller.scrollBottom
+	//var x = this.scroller.scrollBottom
 	message.remove()
 	delete this.messageElements[id]
 	this.totalMessages--
 	
 	if (!parent.firstChild)
 		parent.parentNode.remove()
-	this.scroller.scrollBottom = x
+	//this.scroller.scrollBottom = x
 	
 	return true
 }
