e3bee1a047a5dc706ba1e92d33d184ccb1efd92e
12Me21 <12Me21.mc@gmail.com>
2022-02-16 15:14:10 -0500

    modernize user.js

--- src/Views/user.js
+++ src/Views/user.js
@@ -2,16 +2,17 @@
 with (View) (function($) { "use strict" //*/
 
 addView('user', {
-	start: function(id, query, render) {
+	start(id, query, render) {
 		if (typeof id == 'string' && id[0] == "@")
 			id = id.substr(1)
 		// todo: maybe username without @ should be invalid?
 		// can potentially collide with id numbers
 		
+		let userSearch
 		if (typeof id == 'number')
-			var userSearch = {ids: [id], limit: 1}
+			userSearch = {ids: [id], limit: 1}
 		else
-			var userSearch = {usernames: [id], limit: 1}
+			userSearch = {usernames: [id], limit: 1}
 		
 		return Req.read([
 			{"user": userSearch},
@@ -19,9 +20,9 @@ addView('user', {
 			{"activity.0id$userIds": {limit: 20, reverse: true}},
 			{"commentaggregate.0id$userIds": {limit: 100, reverse: true}},
 			"content.2contentId.3id"
-		], {}, function(e, resp) {
+		], {}, (e, resp)=>{
 			if (!e) {
-				var user = resp.user[0]
+				let user = resp.user[0]
 				if (user)
 					render(user, resp.Puserpage[0], resp.activity, resp.commentaggregate, resp.content)
 				else
@@ -31,13 +32,14 @@ addView('user', {
 		}, true)
 	},
 	className: 'userMode',
-	render: function(user, userpage, activity, ca, content) {
+	render(user, userpage, activity, ca, content) {
 		if (user.id == Req.uid) {
 			flag('myUserPage', true)
+			let path
 			if (userpage) {
-				var path="editpage/"+userpage.id
+				path="editpage/"+userpage.id
 			} else
-				var path="editpage?type=userpage&name="+encodeURIComponent(user.username)+"'s user page"
+				path="editpage?type=userpage&name="+url_escape(user.username)+"'s user page"
 			Nav.link(path, $editUserPage)
 		}
 		setEntityTitle(user)
@@ -49,7 +51,7 @@ addView('user', {
 		else
 			$userPageContents.replaceChildren()
 	},
-	cleanUp: function() {
+	cleanUp() {
 		$userPageAvatar.src = ""
 		$userPageContents.replaceChildren()
 	},
