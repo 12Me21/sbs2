6d3328ab58c361979090c148f128b75b5494e3fe
12Me21 <12Me21.mc@gmail.com>
2022-02-22 05:59:48 -0500

    rsync.

--- src/input.js
+++ src/input.js
@@ -42,7 +42,7 @@
 // nicer replacement for encodeURIComponent
 function url_escape(s) {
 	let escape = c => "%"+(c.charCodeAt(0)+0x100).toString(16).substr(1)
-	return s.replace(/[^-\w\$\.+!*',;/\:@#%~]/g, escape).replace(/[,\.?!:]$/, escape)
+	return s.replace(/[^-\w\$\.+!*',;/\:@~]/g, escape).replace(/[,\.?!:]$/, escape) 
 }
 // todo: also make a full query string encode function
 
