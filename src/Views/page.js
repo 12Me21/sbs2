59b77924a2c13c54e0ff8fdeab6b80407b173b75
12Me21 <12Me21.mc@gmail.com>
2022-03-15 16:51:21 -0400

    clean up Views/page.js (remove global vars)

--- src/Views/page.js
+++ src/Views/page.js
@@ -7,31 +7,19 @@ function register_activity(e) {
 //;['wheel','keydown','mousedown','mousemove','touchstart'].forEach(event => document.addEventListener(event, registerActivity))
 //window.addEventListener('focus', registerActivity)
 
-let track_resize_2 = new ResizeTracker('width')
-
-<!--/* trick indenter
-with (View) (function($) { "use strict" //*/
-
-let room
-
-let render_page = (page)=>{
-	set_entity_title(page)
-	set_entity_path(page.parent)
-	flag('canEdit', /u/.test(page.myPerms))
-	$chatTextarea.disabled = !(page.createUserId==Req.uid || /c/.test(page.permissions[Req.uid] || page.permissions[0]))
-	Nav.link("editpage/"+page.id, $pageEditLink)
-	Nav.link("comments/"+page.id, $pageCommentsLink)
-}
-
-let last_sent = null
-
-add_view('page', {
-	// in this case, we sometimes make a request, and sometimes
-	// load the page instantly because the chatroom is cached
-	// so, call either `render` or `quick`
-	// the function passed to quick will act like a simple view
-	// with no `start` method (is called immediately)
-	// DO NOT CALL BOTH FUNCTIONS!
+View.add_view('page', {
+	room: null, // do we need this?
+	track_resize_2: new ResizeTracker('width'),
+	
+	render_page(page) {
+		View.set_entity_title(page)
+		View.set_entity_path(page.parent)
+		View.flag('canEdit', /u/.test(page.myPerms))
+		$chatTextarea.disabled = !(page.createUserId==Req.uid || /c/.test(page.permissions[Req.uid] || page.permissions[0])) // don't use myperms here
+		Nav.link("editpage/"+page.id, $pageEditLink)
+		Nav.link("comments/"+page.id, $pageCommentsLink)
+	},
+	
 	start(id, query) {
 		let room = ChatRoom.rooms[id]
 		if (room) {
@@ -64,7 +52,7 @@ add_view('page', {
 		//ChatRoom.setViewing([page.id])
 		room.show()
 		room.pinned = z
-		render_page(page)
+		this.render_page(page)
 	},
 	render(resp, ext) {
 		let comments = resp.comment
@@ -72,59 +60,26 @@ add_view('page', {
 		let page = resp.content[0]
 		let pinned = resp.Mpinned
 		
+		// TODO: should we be calling this again every time?
 		Act.new_page_comments(page, comments)
 		Act.redraw()
-		//ChatRoom.setViewing([page.id])
-		room = new ChatRoom(page.id, page)
-		room.display_initial_messages(comments, pinned) //todo: when page is edited, update pinned messages
-		room.show()
 		
-		render_page(page)
+		//ChatRoom.setViewing([page.id])
+		this.room = new ChatRoom(page.id, page)
+		this.room.display_initial_messages(comments, pinned) //todo: when page is edited, update pinned messages
+		this.room.show()
+		
+		this.render_page(page)
 	},
 	cleanup(type) {
-		room && room.hide() //so it's fucking possible for cleanup to get called TWICE if there's an error, sometimes.
+		this.room && this.room.hide() //so it's fucking possible for cleanup to get called TWICE if there's an error, sometimes.
 		room = null
-		flag('canEdit', false)
+		View.flag('canEdit', false)
 	},
 	init() {
-		let send_message = ()=>{
-			let room = ChatRoom.currentRoom
-			if (!room)
-				return
-			
-			let data = read_input(editing_comment, !!editing_comment)
-			if (editing_comment) { // editing comment
-				let last_edit = editing_comment
-				cancel_edit()
-				$chatTextarea.focus()
-				
-				if (data.content) { // input not blank
-					Req.edit_message(last_edit.id, last_edit.parentId, data.content, data.meta).catch((e)=>{
-						alert("Editing comment failed")
-					})
-				} else { // input is blank
-					let resp = confirm("Are you sure you want to delete this message?\n"+last_edit.content)
-					if (resp) {
-						Req.delete_message(last_edit.id).catch((e)=>{
-							alert("Deleting comment failed")
-						})
-					}
-				}
-			} else { // posting new comment
-				if (data.content) { // input is not blank
-					let old = data
-					Req.send_message(room.id, data.content, data.meta).catch(()=>{
-						//error sending message
-						write_input(old)
-					})
-					// going to try this hack to see if that fixes safari
-					window.setTimeout(()=>{
-						$chatTextarea.value = "" //hack?
-						textarea_resize()
-					}, 0)
-				}
-			}
-		}
+		// todo: move this onto the ChatRoom object??
+		window.editComment = this.edit_comment.bind(this) // HACK
+		
 		// up arrow = edit last comment
 		// todo: keep track of the previous message more reliably
 		// either store it when displayed and only pull info from the server if necessary, or idk
@@ -133,13 +88,13 @@ add_view('page', {
 				let room = ChatRoom.currentRoom
 				let msg = room && room.my_last_message()
 				if (msg && msg.x_data)
-					edit_comment(msg.x_data)
+					this.edit_comment(msg.x_data)
 			}
 		}
 		$chatTextarea.onkeypress = (e)=>{
 			if (!e.shiftKey && e.keyCode == 13) { // enter
 				e.preventDefault()
-				send_message()
+				this.send_message()
 			}
 		}
 		// TODO: make sure this is ready when the long poller starts!
@@ -160,86 +115,123 @@ add_view('page', {
 		}
 		
 		$chatCancelEdit.onclick = ()=>{
-			cancel_edit()
+			this.cancel_edit()
 		}
 		// todo: global escape handler?
 		document.addEventListener('keydown', (e)=>{
-			if (e.keyCode == 27) {
-				cancel_edit()
-			}
+			if (e.keyCode == 27)
+				this.cancel_edit()
 		})
 		
-		let textarea_resize = ()=>{
-			$chatTextarea.style.height = ''
-			let height = $chatTextarea.scrollHeight
-			$chatTextarea.parentNode.style.height = $chatTextarea.style.height = height+1+"px"
-		}
-		textarea_resize()
-		$chatTextarea.addEventListener('input', textarea_resize)
-		track_resize_2.add($chatTextarea, ()=>{
-			window.setTimeout(textarea_resize, 0)
+		this.textarea_resize()
+		let r = this.textarea_resize.bind(this)
+		$chatTextarea.addEventListener('input', r)
+		this.track_resize_2.add($chatTextarea, ()=>{
+			window.setTimeout(r, 0)
 		})
-	}
+	},
+	
+	textarea_resize() {
+		$chatTextarea.style.height = ''
+		let height = $chatTextarea.scrollHeight
+		$chatTextarea.parentNode.style.height = $chatTextarea.style.height = height+1+"px"
+	},
+	
+	send_message() {
+		let room = ChatRoom.currentRoom
+		if (!room)
+			return
+		
+		let data = this.read_input(this.editing_comment, !!this.editing_comment)
+		if (this.editing_comment) { // editing comment
+			let last_edit = this.editing_comment
+			this.cancel_edit()
+			$chatTextarea.focus()
+			
+			if (data.content) { // input not blank
+				Req.edit_message(last_edit.id, last_edit.parentId, data.content, data.meta).catch((e)=>{
+					alert("Editing comment failed")
+				})
+			} else { // input is blank
+				let resp = confirm("Are you sure you want to delete this message?\n"+last_edit.content)
+				if (resp) {
+					Req.delete_message(last_edit.id).catch((e)=>{
+						alert("Deleting comment failed")
+					})
+				}
+			}
+		} else { // posting new comment
+			if (data.content) { // input is not blank
+				let old = data
+				Req.send_message(room.id, data.content, data.meta).catch(()=>{
+					//error sending message
+					this.write_input(old)
+				})
+				// going to try this hack to see if that fixes safari
+				window.setTimeout(()=>{
+					$chatTextarea.value = "" //hack?
+					this.textarea_resize()
+				}, 0)
+			}
+		}
+	},
+	
+	read_input(old, editing) {
+		let meta = old ? old.meta : {}
+		
+		if ($chatMarkupSelect.checked)
+			meta.m = Settings.values.chat_markup
+		else
+			meta.m = 'plaintext'
+		if (!editing) {
+			if (Req.me)
+				meta.a = Req.me.avatar
+			if (Settings.values.nickname)
+				meta.n = Entity.filter_nickname(Settings.values.nickname)
+			if (Settings.values.big_avatar=='on' && Settings.values.big_avatar_id)
+				meta.big = Settings.values.big_avatar_id
+		}
+		
+		return {
+			meta: meta,
+			content: $chatTextarea.value,
+		}
+	},
+	
+	write_input(data) {
+		$chatTextarea.value = data.content || ""
+		$chatMarkupSelect.checked = data.meta.m == Settings.values.chat_markup
+		this.textarea_resize()
+	},
+
+	pre_edit: null,
+	editing_comment: null,
+	
+	edit_comment(comment) {
+		if (this.editing_comment)
+			this.cancel_edit()
+		if (!comment)
+			return
+		this.pre_edit = this.read_input()
+		this.editing_comment = comment
+		View.flag('chatEditing', true)
+		this.write_input(comment) // do this after the flag, so the width is right
+		// todo: maybe also save/restore cursor etc.
+		window.setTimeout(x=>{
+			$chatTextarea.setSelectionRange(99999, 99999) // move cursor to end
+			$chatTextarea.focus()
+		},0)
+	},
+	
+	cancel_edit() {
+		if (this.editing_comment) {
+			this.editing_comment = null
+			View.flag('chatEditing', false)
+			this.write_input(this.pre_edit)
+		}
+	},
+	
 })
-
-function read_input(old, edit) {
-	let data = {
-		meta: old ? old.meta : {},
-		content: $chatTextarea.value,
-	}
-	
-	if ($chatMarkupSelect.checked)
-		data.meta.m = Settings.values.chat_markup
-	else
-		data.meta.m = 'plaintext'
-	if (!edit) {
-		if (Req.me)
-			data.meta.a = Req.me.avatar
-		if (Settings.values.nickname)
-			data.meta.n = String(Settings.values.nickname).substr(0, 50).replace(/\n/g, "  ")
-		if (Settings.values.big_avatar=='on' && Settings.values.big_avatar_id)
-			data.meta.big = Settings.values.big_avatar_id
-	}
-	
-	return data
-}
-
-function write_input(data) {
-	$chatTextarea.value = data.content || ""
-	$chatMarkupSelect.checked = data.meta.m == Settings.values.chat_markup
-}
-
-let pre_edit = null
-let editing_comment = null
-
-// todo: move this onto the ChatRoom object??
-$.editComment = edit_comment //HACK
-function edit_comment(comment) {
-	if (editing_comment)
-		cancel_edit()
-	if (!comment)
-		return
-	pre_edit = read_input()
-	editing_comment = comment
-	write_input(comment)
-	View.flag('chatEditing', true)
-	// todo: maybe also save/restore cursor etc.
-	window.setTimeout(x=>{
-		$chatTextarea.setSelectionRange(99999,99999)
-		$chatTextarea.focus()
-	},0)
-}
-
-function cancel_edit() {
-	if (editing_comment) {
-		editing_comment = null
-		View.flag('chatEditing', false)
-		write_input(pre_edit)
-	}
-}
-
-<!--/*
-}(window)) //*/
 
 // when joining a room:
 // - create the ChatRoom object
