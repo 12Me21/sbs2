beb6d946a1639fafe91602b0021d371dc85514cb
12Me21 <12Me21.mc@gmail.com>
2022-06-10 14:36:35 -0400

    pushtag

--- admin/pushtag.sh
+++ admin/pushtag.sh
@@ -1,2 +1,12 @@
-git tag -a "$1" -m "$2"
+#!/bin/sh
+set -ue
+cd -- "`dirname -- "$0"`"
+
+if [ $# -ne 2 ]
+then
+	echo "usage: $0 <tag> <message>"
+	exit
+fi
+
+git tag -a "${1:?}" -m "${2:?}"
 git push origin --tags
