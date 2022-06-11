35de9bccac1b50805e36d8f26241b33285887f33
12Me21 <12Me21.mc@gmail.com>
2022-06-11 19:08:32 -0400

    cleanup styles

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,180 +1,6 @@
 'use strict'
 delete window.sidebar // obsolete firefox global variable
 
-let FileUploader = Object.seal({
-	file: null,
-	last_file: null,
-	file_upload_form: null,
-	
-	onload() {
-		this.file_upload_form = new Form({
-			fields: [
-				['size', 'output', {label: "Size"}], //todo: separate set of output fields?
-				['name', 'text', {label: "File Name"}],
-				['hash', 'text', {label: "Hash"}],
-				['bucket', 'text', {label: "Bucket"}],
-				['quantize', 'select', {
-					options: [null, 2, 4, 8, 16, 32, 64, 256],
-					label: "Quantize",
-					option_labels: ["no", "2", "4", "8", "16", "32", "64", "256"],
-				}],
-			],
-		})
-		$file_upload_form.replaceWith(this.file_upload_form.elem)
-		this.file_cancel()
-		
-		document.addEventListener('paste', e=>{
-			let data = e.clipboardData
-			if (data && data.files) {
-				let file = data.files[0]
-				if (file && (/^image\//).test(file.type))
-					this.got_file(file)
-			}
-		})
-		//todo: write decoder for xpm :)
-		$file_browse.onchange = e=>{
-			let file = $file_browse.files[0]
-			try {
-				file && this.got_file(file)
-			} finally {
-				$file_browse.value = ""
-			}
-		}
-		$file_cancel.onclick = $file_done.onclick = e=>{
-			this.file_cancel()
-		}
-		$file_url_insert.onclick = e=>{
-			let file = this.last_file
-			if (!file) return
-			let url = Req.file_url(file.hash)
-			let meta = JSON.parse(file.meta)
-			let size = meta.width+"x"+meta.height
-			
-			let markup = Settings.values.chat_markup
-			if (markup=='12y')
-				url = "!"+url+"#"+size
-			else if (markup=='12y2')
-				url = "!"+url+"["+size+"]"
-			
-			Sidebar.close_fullscreen()
-			$chatTextarea.focus()
-			document.execCommand('insertText', false, url)
-		}
-		$file_upload.onclick = e=>{
-			if (!this.file) return
-			$file_upload.disabled = true
-			
-			this.file_upload_form.read()
-			let data = this.file_upload_form.get()
-			
-			let params = {
-				tryresize: true,
-				name: data.name || "",
-				values: {},
-			}
-			let priv = false
-			if (data.bucket!=null) {
-				params.values.bucket = data.bucket || ""
-				priv = true
-			}
-			if (data.quantize)
-				params.quantize = data.quantize
-			if (data.hash)
-				params.hash = data.hash
-			if (priv)
-				params.globalPerms = ""
-			print(`uploading ${priv?"private":"public"} file...`)
-			
-			Req.upload_file(this.file, params).do = (file, err)=>{
-				$file_upload.disabled = false
-				if (err) return
-				
-				if (priv && file.permissions[0])
-					alert("file permissions not set correctly!\nid:"+file.id)
-				
-				let url = Req.file_url(file.hash)
-				this.show_parts(2, url, null)
-				$file_url.value = url
-				this.last_file = file
-			}
-		}
-		$file_url.onfocus = e=>{
-			window.setTimeout(()=>{
-				$file_url.select()
-			})
-		}
-	},
-	
-	convert_image(file, quality, callback) {
-		let img = new Image()
-		img.onload = e=>{
-			let canvas = document.createElement('canvas')
-			canvas.width = img.width
-			canvas.height = img.height
-			let c2d = canvas.getContext('2d')
-			c2d.drawImage(img, 0, 0)
-			URL.revokeObjectURL(img.src)
-			let name = file.name
-			file = null
-			let format = quality!=null ? 'jpeg' : 'png'
-			canvas.toBlob(x=>{
-				if (x)
-					x.name = name+"."+format
-				callback(x)
-			}, "image/"+format, quality)
-		}
-		img.onerror = e=>{
-			URL.revokeObjectURL(img.src)
-			callback(null)
-		}
-		img.src = URL.createObjectURL(file)
-	},
-	
-	show_parts(phase, url, file) {
-		$file_browse.hidden = phase!=0
-		$file_cancel.hidden = phase!=1
-		$file_upload.hidden = phase!=1
-		this.file_upload_form.elem.hidden = phase!=1
-		$file_url_insert.hidden = phase!=2
-		$file_url.hidden = phase!=2
-		$file_done.hidden = phase!=2
-		// we set to "" first, so the old image isnt visible whilst the new one is loading
-		$file_image.src = ""
-		if (url)
-			$file_image.src = url
-		this.file = file || null
-	},
-	file_cancel() {
-		this.show_parts(0, null, null)
-	},
-	
-	got_file(file) {
-		if (file.type=='image/webp')
-			return this.convert_image(file, 0.7, x=>{
-				if (!x) {
-					print('image conversion failed!')
-					return
-				}
-				this.got_file(x)
-			})
-		
-		let url = URL.createObjectURL(file)
-		this.show_parts(1, url, file)
-		URL.revokeObjectURL(url)
-		this.file_upload_form.set_some({
-			size: (file.size/1000)+" kB",
-			name: file.name,
-			hash: null,
-		})
-		this.file_upload_form.write()
-		Sidebar.select_tab('file')
-	},
-})
-
-do_when_ready(x=>FileUploader.onload())
-
-
-
 let Sidebar = Object.seal({
 	scroller: null,
 	sidebar_tabs: null,
@@ -393,3 +219,177 @@ let Sidebar = Object.seal({
 window.print = Sidebar.print.bind(Sidebar)
 
 do_when_ready(x=>Sidebar.onload())
+
+
+
+let FileUploader = Object.seal({
+	file: null,
+	last_file: null,
+	file_upload_form: null,
+	
+	onload() {
+		this.file_upload_form = new Form({
+			fields: [
+				['size', 'output', {label: "Size"}], //todo: separate set of output fields?
+				['name', 'text', {label: "File Name"}],
+				['hash', 'text', {label: "Hash"}],
+				['bucket', 'text', {label: "Bucket"}],
+				['quantize', 'select', {
+					options: [null, 2, 4, 8, 16, 32, 64, 256],
+					label: "Quantize",
+					option_labels: ["no", "2", "4", "8", "16", "32", "64", "256"],
+				}],
+			],
+		})
+		$file_upload_form.replaceWith(this.file_upload_form.elem)
+		this.file_cancel()
+		
+		document.addEventListener('paste', e=>{
+			let data = e.clipboardData
+			if (data && data.files) {
+				let file = data.files[0]
+				if (file && (/^image\//).test(file.type))
+					this.got_file(file)
+			}
+		})
+		//todo: write decoder for xpm :)
+		$file_browse.onchange = e=>{
+			let file = $file_browse.files[0]
+			try {
+				file && this.got_file(file)
+			} finally {
+				$file_browse.value = ""
+			}
+		}
+		$file_cancel.onclick = $file_done.onclick = e=>{
+			this.file_cancel()
+		}
+		$file_url_insert.onclick = e=>{
+			let file = this.last_file
+			if (!file) return
+			let url = Req.file_url(file.hash)
+			let meta = JSON.parse(file.meta)
+			let size = meta.width+"x"+meta.height
+			
+			let markup = Settings.values.chat_markup
+			if (markup=='12y')
+				url = "!"+url+"#"+size
+			else if (markup=='12y2')
+				url = "!"+url+"["+size+"]"
+			
+			Sidebar.close_fullscreen()
+			$chatTextarea.focus()
+			document.execCommand('insertText', false, url)
+		}
+		$file_upload.onclick = e=>{
+			if (!this.file) return
+			$file_upload.disabled = true
+			
+			this.file_upload_form.read()
+			let data = this.file_upload_form.get()
+			
+			let params = {
+				tryresize: true,
+				name: data.name || "",
+				values: {},
+			}
+			let priv = false
+			if (data.bucket!=null) {
+				params.values.bucket = data.bucket || ""
+				priv = true
+			}
+			if (data.quantize)
+				params.quantize = data.quantize
+			if (data.hash)
+				params.hash = data.hash
+			if (priv)
+				params.globalPerms = ""
+			print(`uploading ${priv?"private":"public"} file...`)
+			
+			Req.upload_file(this.file, params).do = (file, err)=>{
+				$file_upload.disabled = false
+				if (err) return
+				
+				if (priv && file.permissions[0])
+					alert("file permissions not set correctly!\nid:"+file.id)
+				
+				let url = Req.file_url(file.hash)
+				this.show_parts(2, url, null)
+				$file_url.value = url
+				this.last_file = file
+			}
+		}
+		$file_url.onfocus = e=>{
+			window.setTimeout(()=>{
+				$file_url.select()
+			})
+		}
+	},
+	
+	convert_image(file, quality, callback) {
+		let img = new Image()
+		img.onload = e=>{
+			let canvas = document.createElement('canvas')
+			canvas.width = img.width
+			canvas.height = img.height
+			let c2d = canvas.getContext('2d')
+			c2d.drawImage(img, 0, 0)
+			URL.revokeObjectURL(img.src)
+			let name = file.name
+			file = null
+			let format = quality!=null ? 'jpeg' : 'png'
+			canvas.toBlob(x=>{
+				if (x)
+					x.name = name+"."+format
+				callback(x)
+			}, "image/"+format, quality)
+		}
+		img.onerror = e=>{
+			URL.revokeObjectURL(img.src)
+			callback(null)
+		}
+		img.src = URL.createObjectURL(file)
+	},
+	
+	show_parts(phase, url, file) {
+		$file_browse.hidden = phase!=0
+		$file_cancel.hidden = phase!=1
+		$file_upload.hidden = phase!=1
+		this.file_upload_form.elem.hidden = phase!=1
+		$file_url_insert.hidden = phase!=2
+		$file_url.hidden = phase!=2
+		$file_done.hidden = phase!=2
+		// we set to "" first, so the old image isnt visible whilst the new one is loading
+		$file_image.src = ""
+		if (url)
+			$file_image.src = url
+		this.file = file || null
+	},
+	file_cancel() {
+		this.show_parts(0, null, null)
+	},
+	
+	got_file(file) {
+		if (file.type=='image/webp')
+			return this.convert_image(file, 0.7, x=>{
+				if (!x) {
+					print('image conversion failed!')
+					return
+				}
+				this.got_file(x)
+			})
+		
+		let url = URL.createObjectURL(file)
+		this.show_parts(1, url, file)
+		URL.revokeObjectURL(url)
+		this.file_upload_form.set_some({
+			size: (file.size/1000)+" kB",
+			name: file.name,
+			hash: null,
+		})
+		this.file_upload_form.write()
+		Sidebar.select_tab('file')
+	},
+})
+
+do_when_ready(x=>FileUploader.onload())
