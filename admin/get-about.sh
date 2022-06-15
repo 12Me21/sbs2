87fd60477648071f50247df1a1cf8d389ac4cbcd
12Me21 <12Me21.mc@gmail.com>
2022-06-15 13:42:13 -0400

    ABOUT

--- admin/get-about.sh
+++ admin/get-about.sh
@@ -6,5 +6,8 @@ set -ue ; cd -- "`dirname -- "${BASH_SOURCE:?}"`"
 	curl https://qcs.shsbs.xyz/api/Request/about
 	echo
 } >ABOUT.js
+#sed -E '1i let ABOUT=
+#;s/"([^"\n]+|\\")*":|}/\n&/g'
+
 mv ABOUT.js ../src/ABOUT.js
 echo "done" >&2
