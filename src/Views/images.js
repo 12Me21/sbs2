b972e8c4975bff54b59fcedfa44c5f9af76e944a
12Me21 <12Me21.mc@gmail.com>
2022-06-27 16:23:48 -0400

    change a few..

--- src/Views/images.js
+++ src/Views/images.js
@@ -1,14 +1,14 @@
 'use strict'
 
-const IMAGES_VIEW_PAGINATION = 20
-const IMAGES_VIEW_SIZE = 64
-
 View.add_view('images', {
 	form: null,
 	current: null,
 	page: null,
 	location: null,
 	user: null,
+	SIZE: 60,
+	PER_PAGE: 20,
+	
 	Init() {
 		$imagesWhatever.innerHTML = ''
 		// FIXME: should put this in another function since it's also
@@ -26,26 +26,35 @@ View.add_view('images', {
 		// FIXME: im pretty sure this is something that 12 wanted to be handled
 		// by Draw?
 		// FIXME: this is shit.
-		$imagesNavBack.onclick = () => {
-			if (this.page === undefined || this.page <= 1) return
-			this.location.query.page = String(this.page-1)
+		$imagesPrev.onclick = e=>{go(-1)}
+		$imagesNext.onclick = e=>{go(1)}
+		let go = (dir)=>{
+			let page = this.page || 1
+			if (page+dir<1)
+				return
+			this.location.query.page = page+dir
 			Nav.goto(this.location, true)
 		}
-		$imagesNavForward.onclick = () => {
-			if (this.page === undefined) return
-			this.location.query.page = String(this.page+1)
-			Nav.goto(this.location, true)
-		}
-		$imagesNavBucket.onblur = () => {
+		// should be after enter press or something (wrap in a <form> or whatever, and then the prev/next can be submit buttons?)
+		$imagesNavBucket.onchange = e=>{
 			this.location.query.bucket = $imagesNavBucket.value
 			Nav.goto(this.location, true)
 		}
+		
+		$imagesShowSidebar.onclick = e=>{
+			if (!this.current)
+				return
+			FileUploader.show_content(this.current)
+			Sidebar.select_tab('file')
+		}
+		
 		this.form = new Form({
 			fields: [
 				['user', 'user_output', {label: "User"}],
 				['filename', 'text', {label: "File Name"}],
 				['bucket', 'text', {label: "Bucket"}],
 				['values', 'text', {label: "Values"}], // todo: add an input type for like, json or specifically these values types idk
+				['meta', 'output', {label: "Meta"}],
 				['permissions', 'text', {label: "Permissions"}], //could be a real permission editor but image permissions don't really work anyway
 				//['size', 'output', {output: true, label: "Size"}], I wish
 				// ['quantization', 'output', {label: "Quantization"}],
@@ -54,80 +63,80 @@ View.add_view('images', {
 		$imagesForm.fill(this.form.elem)
 	},
 	Start({id, query}) {
-		const { page=1, bucket=null } = query;
+		const {page=1, bucket=null} = query;
 		// TODO: bucket search
 		return {
 			chain: {
 				values: {
-					contentType: 3,
+					file: 3,
 					key: 'bucket',
-					bucket: `"${bucket}"`
+					bucket: JSON.stringify(bucket),
 				},
 				requests: [
 					{
-						type:'content',
-						fields:'*',
-						query: `contentType = @contentType AND ${bucket ? '!valuelike(@key,@bucket)' : '!valuekeynotlike(@key)'}`,
+						type: 'content',
+						fields: '*',
+						query: `contentType = @file AND ${bucket ? '!valuelike(@key, @bucket)' : '!valuekeynotlike(@key)'}`,
 						order: 'id_desc',
-						limit: IMAGES_VIEW_PAGINATION,
-						skip: (page-1) * IMAGES_VIEW_PAGINATION,
+						limit: this.PER_PAGE,
+						skip: (page-1) * this.PER_PAGE,
 					},
-					{
-						type:'user',
-						fields:'*',
-						query: 'id in @content.createUserId.',
-					}
+					{type:'user', fields:'*', query:"id IN @content.createUserId."},
 				],
 			},
-			ext: {
-				page,
-				bucket
-			}
+			ext: {page, bucket},
 		}
 	},
-	Render({ content=[], user=[] }, { page, bucket }, location) {
+	Render({content, user}, {page, bucket}, location) {
 		this.location = location
-		this.page = parseInt(page)
+		this.page = page|0
 		this.user = user
 		View.set_title(" Images ")
-		content.forEach(x => {
+		content.forEach(x=>{
 			const img = document.createElement('img')
-			img.src = Req.file_url(x.hash, `size=${IMAGES_VIEW_SIZE}`)
-			img.addEventListener('click', y => this.current = x)
+			img.src = Req.file_url(x.hash, `size=${this.SIZE}`)
+			img.onclick = e=>{this.select_image(x)}
+			let meta = JSON.parse(x.meta)
+			/*if (meta.width && meta.height) {
+				img.width = meta.width
+				img.height = meta.height
+			}*/
 			$imagesWhatever.append(img)
 		})
-		
-		$imagesNavPage.textContent = String(page)
+		$imagesNavPage.textContent = page
 		$imagesNavBucket.value = bucket
+		this.select_image(null)
 	},
 	Cleanup(type) {
-		$imagesWhatever.innerHTML = ''
+		this.select_image(null)
+		$imagesWhatever.textContent = ''
 		$imagesNavBucket.value = ''
-		this.current = null
 		this.page = null
 		this.location = null
 		this.form.reset()
-	},	
-	set current(content) {
-		if (content === null) {
-			$imagesCurrentImg.src = ''
-			$imagesCurrentLink.href = ''
-			$imagesCurrentLink.textContent = ''
+	},
+	select_image(content) {
+		this.current = content
+		$imagesCurrentImg.src = "" // always set this, otherwise the old image will be visible until the new one loads
+		if (!content) {
+			$imagesCurrentLink.href = ""
+			$imagesCurrentLink.textContent = ""
 			return
 		}
 		$imagesCurrentImg.src = Req.file_url(content.hash)
 		$imagesCurrentLink.href = `#file/${content.id}`
 		$imagesCurrentLink.textContent = content.hash
 		const formData = {
-			user: this.user?.find(u => u.id === content.createUserId),
+			user: this.user[~content.createUserId],
 			filename: content.name,
-			bucket: content.values?.bucket,
+			bucket: content.values.bucket,
 			// fixme: what is this for exactly?
 			values: JSON.stringify(content.values),
+			meta: content.meta,
 			permissions: JSON.stringify(content.permissions)
 		}
-		this.form?.set(formData)
-		this.form?.write(formData)
+		this.form.set(formData)
+		this.form.write()
 	},
 })
 
