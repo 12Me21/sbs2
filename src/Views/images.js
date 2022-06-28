94d23815f0b2bdd66f0b85d7127e17234baf0269
12Me21 <12Me21.mc@gmail.com>
2022-06-27 20:52:48 -0400

    convert images/

--- src/Views/images.js
+++ src/Views/images.js
@@ -1,19 +1,17 @@
 'use strict'
 
-View.add_view('images', {
-	form: null,
-	current: null,
-	page: null,
-	location: null,
-	user: null,
-	SIZE: 60,
-	PER_PAGE: 20,
-	
+class ImagesView extends BaseView {
 	Init() {
-		$imagesWhatever.innerHTML = ''
+		this.current = null
+		this.page = null
+		this.user = null
+		this.SIZE = 60
+		this.PER_PAGE = 20
+		
+		this.$imagesWhatever.innerHTML = ''
 		// FIXME: should put this in another function since it's also
 		// done in the file view
-		$imagesAvatarButton.onclick = () => {
+		this.$imagesAvatarButton.onclick = () => {
 			if (!this.current) return
 			Req.me.avatar = this.current.hash
 			Req.write(Req.me).do = (resp, err)=>{
@@ -26,8 +24,8 @@ View.add_view('images', {
 		// FIXME: im pretty sure this is something that 12 wanted to be handled
 		// by Draw?
 		// FIXME: this is shit.
-		$imagesPrev.onclick = e=>{go(-1)}
-		$imagesNext.onclick = e=>{go(1)}
+		this.$imagesPrev.onclick = e=>{go(-1)}
+		this.$imagesNext.onclick = e=>{go(1)}
 		let go = (dir)=>{
 			let page = this.page || 1
 			if (page+dir<1)
@@ -36,12 +34,12 @@ View.add_view('images', {
 			Nav.goto(this.location, true)
 		}
 		// should be after enter press or something (wrap in a <form> or whatever, and then the prev/next can be submit buttons?)
-		$imagesNavBucket.onchange = e=>{
-			this.location.query.bucket = $imagesNavBucket.value
+		this.$imagesNavBucket.onchange = e=>{
+			this.location.query.bucket = this.$imagesNavBucket.value
 			Nav.goto(this.location, true)
 		}
 		
-		$imagesShowSidebar.onclick = e=>{
+		this.$imagesShowSidebar.onclick = e=>{
 			if (!this.current)
 				return
 			FileUploader.show_content(this.current)
@@ -60,8 +58,8 @@ View.add_view('images', {
 				// ['quantization', 'output', {label: "Quantization"}],
 			]
 		})
-		$imagesForm.fill(this.form.elem)
-	},
+		this.$imagesForm.fill(this.form.elem)
+	}
 	Start({id, query}) {
 		const {page=1, bucket=null} = query;
 		// TODO: bucket search
@@ -86,7 +84,7 @@ View.add_view('images', {
 			},
 			ext: {page, bucket},
 		}
-	},
+	}
 	Render({content, user}, {page, bucket}, location) {
 		this.location = location
 		this.page = page|0
@@ -101,31 +99,23 @@ View.add_view('images', {
 				img.width = meta.width
 				img.height = meta.height
 			}*/
-			$imagesWhatever.append(img)
+			this.$imagesWhatever.append(img)
 		})
-		$imagesNavPage.textContent = page
-		$imagesNavBucket.value = bucket
+		this.$imagesNavPage.textContent = page
+		this.$imagesNavBucket.value = bucket
 		this.select_image(null)
-	},
-	Cleanup(type) {
-		this.select_image(null)
-		$imagesWhatever.textContent = ''
-		$imagesNavBucket.value = ''
-		this.page = null
-		this.location = null
-		this.form.reset()
-	},
+	}
 	select_image(content) {
 		this.current = content
-		$imagesCurrentImg.src = "" // always set this, otherwise the old image will be visible until the new one loads
+		this.$imagesCurrentImg.src = "" // always set this, otherwise the old image will be visible until the new one loads
 		if (!content) {
-			$imagesCurrentLink.href = ""
-			$imagesCurrentLink.textContent = ""
+			this.$imagesCurrentLink.href = ""
+			this.$imagesCurrentLink.textContent = ""
 			return
 		}
-		$imagesCurrentImg.src = Req.file_url(content.hash)
-		$imagesCurrentLink.href = `#file/${content.id}`
-		$imagesCurrentLink.textContent = content.hash
+		this.$imagesCurrentImg.src = Req.file_url(content.hash)
+		this.$imagesCurrentLink.href = `#file/${content.id}`
+		this.$imagesCurrentLink.textContent = content.hash
 		const formData = {
 			user: this.user[~content.createUserId],
 			filename: content.name,
@@ -137,6 +127,28 @@ View.add_view('images', {
 		}
 		this.form.set(formData)
 		this.form.write()
-	},
-})
+	}
+}
 
+ImagesView.template = HTML`
+<div class='COL'>
+	<div $=imagesWhatever class='images-thumbnails'></div>
+	<div $=imagesNav class='nav ROW rem1-5'>
+		<button $=imagesPrev>◀prev</button>
+		<span $=imagesNavPage class='textItem'>0</span>
+		<button $=imagesNext>next▶</button>
+		<input $=imagesNavBucket placeholder="bucket">
+	</div>
+	<div $=imagesCurrent class='FILL ROW'>
+		<div $=imagesCurrentImgContainer class='FILL images-container'>
+			<img $=imagesCurrentImg class='images-current'>
+		</div>
+		<div style="width:50%">
+			<a $=imagesCurrentLink></a><button $=imagesAvatarButton>Set Avatar</button><button $=imagesShowSidebar>show in sidebar</button>
+			<div $=imagesForm></div>
+		</div>
+	</div>
+</div>
+`
+
+ImagesView.define('images')
