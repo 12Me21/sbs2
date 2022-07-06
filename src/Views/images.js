43e1e4ae7c09460dc1a7b449ead04bc020f28ded
12Me21 <12Me21.mc@gmail.com>
2022-07-06 03:38:29 -0400

    fix/cleanup image viewer

--- src/Views/images.js
+++ src/Views/images.js
@@ -1,65 +1,12 @@
 'use strict'
 
 class ImagesView extends BaseView {
-	Init() {
+	Start(location) {
 		this.current = null
-		this.page = null
 		this.user = null
 		
-		this.$imagesWhatever.innerHTML = ''
-		// FIXME: should put this in another function since it's also
-		// done in the file view
-		this.$imagesAvatarButton.onclick = () => {
-			if (!this.current) return
-			Req.me.avatar = this.current.hash
-			Req.write(Req.me).do = (resp, err)=>{
-				if (!err)
-					print('ok')
-				else
-					alert('edit failed')
-			}
-		}
-		// FIXME: im pretty sure this is something that 12 wanted to be handled
-		// by Draw?
-		// FIXME: this is shit.
-		this.$imagesPrev.onclick = e=>{go(-1)}
-		this.$imagesNext.onclick = e=>{go(1)}
-		let go = (dir)=>{
-			let page = this.page || 1
-			if (page+dir<1)
-				return
-			this.location.query.page = page+dir
-			Nav.goto(this.location, true)
-		}
-		// should be after enter press or something (wrap in a <form> or whatever, and then the prev/next can be submit buttons?)
-		this.$imagesNavBucket.onchange = e=>{
-			this.location.query.bucket = this.$imagesNavBucket.value
-			Nav.goto(this.location, true)
-		}
-		
-		this.$imagesShowSidebar.onclick = e=>{
-			if (!this.current)
-				return
-			FileUploader.show_content(this.current)
-			Sidebar.select_tab('file')
-		}
-		
-		this.form = new Form({
-			fields: [
-				['user', 'user_output', {label: "User"}],
-				['filename', 'text', {label: "File Name"}],
-				['values', 'text', {label: "Values"}], // todo: add an input type for like, json or specifically these values types idk
-				['meta', 'output', {label: "Meta"}],
-				['permissions', 'text', {label: "Permissions"}], //could be a real permission editor but image permissions don't really work anyway
-				//['size', 'output', {output: true, label: "Size"}], I wish
-				// ['quantization', 'output', {label: "Quantization"}],
-			]
-		})
-		this.$imagesForm.fill(this.form.elem)
-	}
-	Start(location) {
-		let bucket = location.query.bucket;
-		let page = (location.query.page|0) || 1;
+		let bucket = location.query.bucket
+		let page = (location.query.page|0) || 1
 		// TODO: bucket search
 		let search = "contentType = @file"
 		if (bucket)
@@ -80,6 +27,58 @@ class ImagesView extends BaseView {
 			},
 		}
 	}
+	Init() {
+		this.$whatever.innerHTML = ''
+		// FIXME: should put this in another function since it's also
+		// done in the file view
+		this.$set_avatar.onclick = () => {
+			if (!this.current) return
+			Req.me.avatar = this.current.hash
+			Req.write(Req.me).do = (resp, err)=>{
+				if (!err)
+					print('ok')
+				else
+					alert('edit failed')
+			}
+		}
+		// FIXME: im pretty sure this is something that 12 wanted to be handled
+		// by Draw?
+		// FIXME: this is shit.
+		this.$prev.onclick = e=>{go(-1)}
+		this.$next.onclick = e=>{go(1)}
+		let go = (dir)=>{
+			let page = this.page || 1
+			if (page+dir<1)
+				return
+			this.location.query.page = page+dir
+			Nav.goto(this.location, true)
+		}
+		// should be after enter press or something (wrap in a <form> or whatever, and then the prev/next can be submit buttons?)
+		this.$bucket.onchange = e=>{
+			this.location.query.bucket = this.$bucket.value||null
+			Nav.goto(this.location, true)
+		}
+		
+		this.$in_sidebar.onclick = e=>{
+			if (!this.current)
+				return
+			FileUploader.show_content(this.current)
+			Sidebar.select_tab('file')
+		}
+		
+		this.form = new Form({
+			fields: [
+				['user', 'user_output', {label: "User"}],
+				['filename', 'text', {label: "File Name"}],
+				['values', 'text', {label: "Values"}], // todo: add an input type for like, json or specifically these values types idk
+				['meta', 'output', {label: "Meta"}],
+				['permissions', 'text', {label: "Permissions"}], //could be a real permission editor but image permissions don't really work anyway
+				//['size', 'output', {output: true, label: "Size"}], I wish
+				// ['quantization', 'output', {label: "Quantization"}],
+			]
+		})
+		this.$data.fill(this.form.elem)
+	}
 	Render({content, user}) {
 		this.user = user
 		View.set_title(" Images ")
@@ -88,23 +87,24 @@ class ImagesView extends BaseView {
 			img.src = Req.file_url(file.hash, "size=60")
 			img.onclick = e=>{this.select_image(file)}
 			//let meta = JSON.parse(file.meta)
-			this.$imagesWhatever.append(img)
+			this.$whatever.append(img)
 		}
-		this.$imagesNavPage.textContent = this.page
-		this.$imagesNavBucket.value = this.bucket
+		this.$page.textContent = this.page
+		this.$bucket.value = this.bucket||""
 		this.select_image(null)
 	}
+	
 	select_image(content) {
 		this.current = content
-		this.$imagesCurrentImg.src = "" // always set this, otherwise the old image will be visible until the new one loads
+		this.$image.src = "" // always set this, otherwise the old image will be visible until the new one loads
 		if (!content) {
-			this.$imagesCurrentLink.href = ""
-			this.$imagesCurrentLink.textContent = ""
+			this.$image_link.href = ""
+			this.$image_link.textContent = ""
 			return
 		}
-		this.$imagesCurrentImg.src = Req.file_url(content.hash)
-		this.$imagesCurrentLink.href = `#page/${content.id}`
-		this.$imagesCurrentLink.textContent = content.hash
+		this.$image.src = Req.file_url(content.hash)
+		this.$image_link.href = `#page/${content.id}`
+		this.$image_link.textContent = content.hash
 		this.form.set({
 			user: this.user[~content.createUserId],
 			filename: content.name,
@@ -118,20 +118,22 @@ class ImagesView extends BaseView {
 
 ImagesView.template = HTML`
 <view-root class='COL'>
-	<div $=imagesWhatever class='images-thumbnails'></div>
-	<div $=imagesNav class='nav ROW rem1-5'>
-		<button $=imagesPrev>◀prev</button>
-		<span $=imagesNavPage class='textItem'>0</span>
-		<button $=imagesNext>next▶</button>
-		<input $=imagesNavBucket placeholder="bucket">
+	<div $=whatever class='images-thumbnails'></div>
+	<div class='nav ROW rem1-5'>
+		<button $=prev>◀prev</button>
+		<span $=page class='textItem'>0</span>
+		<button $=next>next▶</button>
+		<input $=bucket placeholder="bucket">
 	</div>
-	<div $=imagesCurrent class='FILL ROW'>
-		<div $=imagesCurrentImgContainer class='FILL images-container'>
-			<img $=imagesCurrentImg class='images-current'>
+	<div class='FILL ROW'>
+		<div class='FILL images-container'>
+			<img $=image class='images-current'>
 		</div>
 		<div style="width:50%">
-			<a $=imagesCurrentLink></a><button $=imagesAvatarButton>Set Avatar</button><button $=imagesShowSidebar>show in sidebar</button>
-			<div $=imagesForm></div>
+			<a $=image_link></a>
+			<button $=set_avatar>Set Avatar</button>
+			<button $=in_sidebar>show in sidebar</button>
+			<div $=data></div>
 		</div>
 	</div>
 </view-root>
