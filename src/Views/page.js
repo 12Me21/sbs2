868980ba79c4381c114541238dd03d65ce66fd10
12Me21 <12Me21.mc@gmail.com>
2022-07-23 04:31:36 -0400

    I'm sorry

--- src/Views/page.js
+++ src/Views/page.js
@@ -61,12 +61,6 @@ class PageView extends BaseView {
 				this.edit_comment(null)
 		}
 		
-		this.$collapse.onchange = ev=>{
-			this.$page_contents.toggleAttribute('data-collapse', this.$collapse.checked)
-			if (ev)
-				localStorage.setItem('setting--collapse-sections-'+this.page_id, this.$collapse.checked ? 'yes' : '')
-		}
-		
 		this.$watching.onchange = Draw.event_lock(done=>{
 			// todo: check for err
 			Req.set_watch(this.page_id, this.$watching.checked).do = resp=>{
@@ -104,10 +98,6 @@ class PageView extends BaseView {
 		this.list = new MessageList(this.$message_list, this.page_id)
 		this.pinned_list = null
 		
-		if (localStorage.getItem('setting--collapse-sections-'+this.page_id)) {
-			this.$collapse.checked = true
-			this.$collapse.onchange(null)
-		}
 		// draw stuff //
 		this.update_page(page, user)
 		this.update_watch(watch[0])
@@ -362,7 +352,6 @@ PageView.template = HTML`
 			<label>Watching: <input type=checkbox $=watching></label>
 			<span $=author style='margin: 0 0.5rem;'></span>
 			<span $=create_date>Created: <time></time></span>
-			|<label>collapse: <input type=checkbox $=collapse></label>
 		</div>
 		<div class='pageContents' $=page_contents></div>
 	</scroll-outer>
