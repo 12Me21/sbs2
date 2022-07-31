6917a9468f400a8725725169e3c6db6d63e2dd92
12Me21 <12Me21.mc@gmail.com>
2022-07-31 19:55:59 -0400

    revert

--- src/view.js
+++ src/view.js
@@ -272,8 +272,8 @@ const View = NAMESPACE({
 			if (element==this.$embiggened)
 				element = null // already big: make small
 			this.set_embiggened(element)
-			ev.preventDefault()
-		})
+			//ev.preventDefault()
+		}, {passive:true})
 		
 		// clicking outside an image shrinks it
 		// maybe could block this if the click is on a link/button?
