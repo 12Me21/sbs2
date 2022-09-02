35fbb29419ddb9c2f3075fafc42f2002d3100f42
ilovecherries <cherry@epochal.quest>
2022-09-02 08:04:44 -0400

    Initially updating the template for Views
    I should maybe document the return type of `Start' and verify if my
    claim in `Destroy' is true.

--- src/Views/template.js
+++ src/Views/template.js
@@ -1,26 +1,63 @@
-View.add_view('template', {
-	init() {
-		// this is called when the page initially loads
-		// (in the future, it may be deferred until the view is visited
-		// for the first time)
-	},
-	start(id, query, render, quick) {
-		// this should make a request for data from the api
-		// and call `render` when it's finished
-		// DO NOT MODIFY ANY HTML IN THIS FUNCTION
-		// If you don't need to load anything asynchronously,
-		// you can leave out this function and `render` will be
-		// called immediately instead (with arguments (id, query, type))
-		
-		// or, you can call `quick` instead of `render`
-	},
-	className: 'template', // <v-b data-view-template> blocks will be visible, and all other <v-b> blocks are hidden
-	render() {
-		// this function is called after the data is recieved, and
-		// should render the page
-	},
-	cleanup() {
-		// this is called before switching to another page,
-		// to remove any unneeded content that was created by `render`
-	},
-})
+'use strict'
+
+/**
+ * `route_name'/`id'?`query'
+ * @typedef {Object} Location
+ * @property {string} type - Not known yet
+ * @property {number|string} id - The ID of the location (usually a hash or number ID)
+ * @property {Object} query - Key/Value params passed to the path
+ * @property {unknown} fragment - I think this is the anchor?
+ */
+
+// Naming convention is {Name}View.
+class TemplateView extends BaseView {
+	/**
+	 * This is run when the page is initially loaded. This should be used for
+	 * setup that should only be run once.
+	 * (in the future, it may be deferred until the view is visited
+	 * for the first time)
+	 */
+	Init() {
+		// Fill me in.
+	}
+
+	/**
+	 * This is used every time the View is loaded in order to fetch an API request.
+	 * @param {Location} location - The location
+	 */
+	Start({id, query}) {
+		return {
+			chain: {
+				values: {},
+				requests: [],
+			},
+			check: resp=>true
+		}
+	}
+	
+	/**
+	 * This is run after the API request that is triggered from `Init' is completed
+	 * This should be used to update the HTML on the page.
+	 * @param {Object} response - The response given back from the API declared by `Init'.
+	 */
+	Render(resp) {
+		// Fill me in
+	}
+
+	/**
+	  * This is used before unloading the View in order to clean it up.
+	  * (I believe a View will not actually fully delete all of the HTML if you
+	  *  only change one of the properties passed to it, so it should be done even
+	  *  if it's unnecessary? Best to consult with 12 on this one...)
+	  */
+	Destroy() {
+		// Fill me in
+	}
+}
+
+TemplateView.template = HTML`
+<view-root class='COL'>
+   Hello, World!
+</view-root>
+`
+
