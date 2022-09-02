ba92d33103a981bdfe3a4c5af5d1850795d14903
ilovecherries <cherry@epochal.quest>
2022-09-02 08:24:25 -0400

    Finished View template
    >w<

--- src/Views/template.js
+++ src/Views/template.js
@@ -9,6 +9,13 @@
  * @property {unknown} fragment - I think this is the anchor?
  */
 
+/**
+ * Chain request
+ * @typedef {Object} ChainRequest
+ * @property {boolean} [quick] - Used to determine whether `Quick' or `Render' should be run. True=Quick, False=Render
+ * @property {Object} [chain] - The API request parameters.
+ */
+
 // Naming convention is {Name}View.
 class TemplateView extends BaseView {
 	/**
@@ -26,6 +33,11 @@ class TemplateView extends BaseView {
 	 * @param {Location} location - The location
 	 */
 	Start({id, query}) {
+		const return_quick = true
+		// emacs doesn't know how to indent tertiary conditionals correctly? lol
+		// sorry for this abysmal stuff....
+		if (return_quick)
+			return { quick: true }
 		return {
 			chain: {
 				values: {},
@@ -33,6 +45,17 @@ class TemplateView extends BaseView {
 			},
 			check: resp=>true
 		}
+	}
+
+	/**
+	 * This is run when the return value of `Start' has `quick' set to true instead
+	 * of render.
+	 * An API request is also not passed to ContentAPI. This is typically used in the
+	 * case where you do not need an API request. (whether it is because of something
+	 * already being cached or it does not require a request or anything similar)
+	 */
+	Quick() {
+		// Fill me in
 	}
 	
 	/**
@@ -45,11 +68,19 @@ class TemplateView extends BaseView {
 	}
 
 	/**
-	  * This is used before unloading the View in order to clean it up.
-	  * (I believe a View will not actually fully delete all of the HTML if you
-	  *  only change one of the properties passed to it, so it should be done even
-	  *  if it's unnecessary? Best to consult with 12 on this one...)
-	  */
+	 * When the View becomes visible. Triggered when initially viewing
+	 * the View or when viewing the View again after being in a "hidden" state.
+	 * (caused by being in another tab or having the window out of view)
+	 */
+	Visible() {
+		// Fill me in
+	}
+
+	/**
+	 * When loading a new view (in the case where one of the parameters passed to
+	 * `Start' is changed), this method is ran in order to clean it up before rerendering
+	 * onto the view again.
+	 */
 	Destroy() {
 		// Fill me in
 	}
@@ -61,3 +92,4 @@ TemplateView.template = HTML`
 </view-root>
 `
 
+View.register('template', TemplateView)
